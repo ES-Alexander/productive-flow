6ce033f83bae30b5d615c0ec31f66153882dc030
ES Alexander <sandman.esalexander@gmail.com>
2020-03-13 08:58:52 +1100

    cli for dump, can specify project to add to

--- dump.py
+++ dump.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 
 from project import Project
+import os, sys
 
-main = Project('main')
+argc = len(sys.argv)
+name = 'main'
+path = 'projects'
+if argc > 1:
+    name = sys.argv[1]
+    if argc > 2:
+        path = sys.argv[2]
+
+if '/' in path and not os.path.isfile(path + '/{}.txt'.format(name)):
+    # parent needs to know about new sub_project being added to
+    prev_name_ind = path.rindex('/')
+    prev_name = path[prev_name_ind:]
+    prev_path = path[:prev_name_ind]
+    prev = Project(prev_name, path=prev_path)
+    proj = prev.create_sub_project(name, path=path)
+else:
+    # adding to main, or adding to known sub_project
+    proj = Project(name, path=path)
 
 section = '-' * 50
 print('', '#' * 50, '',
@@ -35,8 +53,8 @@ while True:
         info += elaboration + '\n'
         elaboration = input()
 
-    main.create_sub_project(name, **eval('dict({})'.format(info)))
+    proj.create_sub_project(name, **eval('dict({})'.format(info)))
 
-main.save()
+proj.save()
 print('New items saved\n')
-main.print()
+proj.print()
